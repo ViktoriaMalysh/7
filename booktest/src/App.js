342409970b3a53f6a5fa607957779ec97e24c0a9
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2022-02-12 18:54:16 +0200

    v7

--- booktest/src/App.js
+++ booktest/src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import { connect, useDispatch, useSelector } from "react-redux";
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { addCount } from './store/roomSlice';
-
-
+import { addCount, setStatus } from './store/roomSlice';
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime"
 const defaultState = [
   { id: 1, count: 0, status: "free" },
   { id: 2, count: 0, status: "free" },
@@ -19,37 +19,73 @@ const defaultState = [
 
 function App() {
   const dispatch = useDispatch();
-  const store = useSelector((state) => state.rooms);
-
-  const persistStore = useSelector((state) => state.persist);
+  const rooms = useSelector(state => state.rooms.rooms);
   const [flag, setFlag] = useState(false);
   const [arr, setArr] = useState(defaultState);
-  console.log(arr);
-  console.log('persistStore', persistStore);
+
+  const [count, setCount] = useState(0);
+  // console.log(arr);
+  // console.log('persistStore', persistStore);
+
+  // useEffect(() => {
+  //   if (flag) {
+  //     let interval = null;
+  //     interval = setInterval(() => {
+  //       setArr(
+  //         arr.map((item) => {
+  //           if (item.count > 0) {
+  //             item.count = item.count - 1000;
+  //           } 
+  //           return item;
+  //         })
+  //       );
+  //     }, 1000);
+
+  //     return () => clearInterval(interval);
+      
+  //   }
+  // }, [arr.count, flag]);
+
+  // useEffect(() => {
+  //   const p = setFlag(arr.some((elem) => elem.status === "reserved"));
+  //   if (!p) setFlag(false);
+  // }, [arr.status]);
 
   useEffect(() => {
-    if (flag) {
+    if(count!==10){
+      console.log('persistStore', rooms)
       let interval = null;
       interval = setInterval(() => {
-        setArr(
-          arr.map((item) => {
-            if (item.count > 0) {
-              item.count = item.count - 1000;
+        
+          rooms.map((item) => {
+            if (item.status === 'reserved') {
+              let current = dayjs()
+              // // current = dayjs(current.$d).format("hh mm ss");
+              // console.log('item.end', item.end)
+              // let currentTime = dayjs(item.end).subtract(dayjs(current))
+              // console.log('currentTime', currentTime)
+              // setCount(count+1)
+              // dayjs.extend(relativeTime)
+
+              // let time = dayjs(item.end).fromNow() // 20 years ago
+              // let duration = dayjs.duration(item.end.diff(current)).milliseconds()
+
+              // let time = item.end - item.start
+              let time = dayjs(item.end).subtract(dayjs(current))
+              time = dayjs(time.$d).format("hh mm ss");
+              console.log('time', time)
+              setCount(count+1)
             } 
             return item;
           })
-        );
+      
       }, 1000);
 
       return () => clearInterval(interval);
-      
-    }
-  }, [arr.count, flag]);
+      // let currentTime = dayjs(end).subtract(dayjs(start))
 
-  useEffect(() => {
-    const p = setFlag(arr.some((elem) => elem.status === "reserved"));
-    if (!p) setFlag(false);
-  }, [arr.status]);
+    }
+  }, [count])
 
   const handleBookRoom = () => {
     setArr(
@@ -71,23 +107,38 @@ function App() {
       })
     );
 
-    console.log('newArr', arr)
+    // console.log('newArr', arr)
   };
 
   const handleBook = (room) => {
-    // dispatch({ type: ID, payload: room.id });
-    setArr(
-      arr.map((item) => {
+
+    // setArr(
+      rooms.map((item) => {
         if (item.id === room.id) {
-          item.count = 120000;
-          item.status = "reserved";
-          setFlag(true);
+          const id = item.id
+          dispatch(setStatus({id}))
+          // item.status = "reserved";
+          // setFlag(true);
         }
         return item;
       })
-    );
-    const id = 1
-    dispatch(addCount({id}))
+    // );
+    let start = dayjs()
+    let end = dayjs().add(2, 'minute')
+    // start = dayjs(start.$d).format("hh mm ss");
+    // end = dayjs(end.$d).format("hh mm ss");
+
+    // let currentTime = dayjs(end).subtract(dayjs(start))
+    const bookRoom = {
+      id: room.id,
+      start: start,
+      end: end
+    }
+    // console.log('start', start)
+    // console.log('end', end)
+    // console.log('currentTime', currentTime)
+    dispatch(addCount({bookRoom}))
+    setCount(1)
   };
 
   return (
--- booktest/src/store/roomSlice.js
+++ booktest/src/store/roomSlice.js
@@ -4,26 +4,34 @@ const roomSlice = createSlice({
   name: "rooms",
   initialState: {
     rooms: [
-      { id: 1, counter: 0, status: "free" },
-      { id: 2, counter: 0, status: "free" },
-      { id: 3, counter: 0, status: "free" },
-      { id: 4, counter: 0, status: "free" },
-      { id: 5, counter: 0, status: "free" },
-      { id: 6, counter: 0, status: "free" },
-      { id: 7, counter: 0, status: "free" },
-      { id: 8, counter: 0, status: "free" },
-      { id: 9, counter: 0, status: "free" },
+      { id: 1, start: '', end: '', status: "free" },
+      { id: 2, start: '', end: '', status: "free" },
+      { id: 3, start: '', end: '', status: "free" },
+      { id: 4, start: '', end: '', status: "free" },
+      { id: 5, start: '', end: '', status: "free" },
+      { id: 6, start: '', end: '', status: "free" },
+      { id: 7, start: '', end: '', status: "free" },
+      { id: 8, start: '', end: '', status: "free" },
+      { id: 9, start: '', end: '', status: "free" },
     ],
   },
   reducers: {
     addCount(state, action) {
-      console.log(action.payload.id);
+      console.log(action.payload.bookRoom.id);
       state.rooms.map((room) => {
-        if (room.id === action.payload.id) {
-          Object.assign(room, { counter: 120000 });
+        if (room.id === action.payload.bookRoom.id) {
+          Object.assign(room, { start: action.payload.bookRoom.start, end: action.payload.bookRoom.end });
         }
       });
     },
+    setStatus(state, action) {
+        console.log(action.payload.id);
+        state.rooms.map((room) => {
+          if (room.id === action.payload.id) {
+            Object.assign(room, { status: 'reserved' });
+          }
+        });
+      },
     toggleComplete(state, action) {
       const toggledTodo = state.todos.find(
         (todo) => todo.id === action.payload.id
@@ -36,6 +44,6 @@ const roomSlice = createSlice({
   },
 });
 
-export const { addCount, toggleComplete, removeTodo } = roomSlice.actions;
+export const { addCount, setStatus, toggleComplete, removeTodo } = roomSlice.actions;
 
 export default roomSlice.reducer;
